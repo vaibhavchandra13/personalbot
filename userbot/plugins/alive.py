1d7177743d0179a90a613bfcb46ebc87410ef841
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2021-07-01 21:32:32 +0530

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,107 @@
+
+# Thanks to Sipak bro and Aryan.. 
+# animation Idea by @NOOB_GUY_OP (Sipakisking) && @Hell boy_pikachu
+# Made by @ROMANTIC_KILLER...and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+# Porting in Mafia Userbot by @H1M4N5HU0P
+
+import asyncio
+import random
+from telethon import events, version
+from userbot import ALIVE_NAME, mafiaversion
+from userbot.utils import admin_cmd, sudo_cmd
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot.cmdhelp import CmdHelp
+
+# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋"
+
+# Thanks to Sipak bro and Raganork.. 
+# animation Idea by @NOOB_GUY_OP (Sipakisking)
+# Made by @ROMANTIC_KILLER...and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+# alive.py for ÂÝŮ$HópBØȚ
+
+ludosudo = Config.SUDO_USERS
+
+if ludosudo:
+    sudou = "True"
+else:
+    sudou = "False"
+
+mafia = bot.uid
+
+edit_time = 16
+""" =======================CONSTANTS====================== """
+file1 = "https://telegra.ph/file/4611d9629a8c64896992f.jpg"
+file2 = "https://telegra.ph/file/4611d9629a8c64896992f.jpg"
+file3 = "https://telegra.ph/file/4611d9629a8c64896992f.jpg"
+file4 = "https://telegra.ph/file/4611d9629a8c64896992f.jpg"
+""" =======================CONSTANTS====================== """
+pm_caption = "  __**𝖂⭕⎝⨏  OP**__\n\n"
+
+pm_caption += f"**ᴀʟɪᴠᴇ**\n"
+pm_caption += f"Pᴇʀsᴏ̨ɴᴀl Bᴏᴛ\n "
+pm_caption += f"Monxter's Don't Sleep Under Your Bed 😈 They Sleep inside Your Head🤯\n"
+pm_caption += "Gʀᴇyωøℓƒxᴅ"
+
+# @command(outgoing=True, pattern="^.alive$")
+@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
+@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+async def amireallyalive(alive):
+    await alive.get_chat()   
+    await alive.delete()
+    on = await borg.send_file(alive.chat_id, file=file1,caption=pm_caption)
+
+    await asyncio.sleep(edit_time)
+    ok = await borg.edit_message(alive.chat_id, on, file=file2) 
+
+    await asyncio.sleep(edit_time)
+    ok2 = await borg.edit_message(alive.chat_id, ok, file=file3)
+
+    await asyncio.sleep(edit_time)
+    ok3 = await borg.edit_message(alive.chat_id, ok2, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok4 = await borg.edit_message(alive.chat_id, ok3, file=file3)
+    
+    await asyncio.sleep(edit_time)
+    ok5 = await borg.edit_message(alive.chat_id, ok4, file=file2)
+    
+    await asyncio.sleep(edit_time)
+    ok6 = await borg.edit_message(alive.chat_id, ok5, file=file4)
+    
+    await asyncio.sleep(edit_time)
+    ok7 = await borg.edit_message(alive.chat_id, ok6, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok8 = await borg.edit_message(alive.chat_id, ok7, file=file2) 
+
+    await asyncio.sleep(edit_time)
+    ok9 = await borg.edit_message(alive.chat_id, ok8, file=file3)
+
+    await asyncio.sleep(edit_time)
+    ok10 = await borg.edit_message(alive.chat_id, ok9, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok11 = await borg.edit_message(alive.chat_id, ok10, file=file3)
+    
+    await asyncio.sleep(edit_time)
+    ok12 = await borg.edit_message(alive.chat_id, ok11, file=file2)
+    
+    await asyncio.sleep(edit_time)
+    ok13 = await borg.edit_message(alive.chat_id, ok12, file=file4)
+    
+    await asyncio.sleep(edit_time)
+    ok14 = await borg.edit_message(alive.chat_id, on, file=file1)
+
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, caption=pm_caption)
+    await alive.delete()
+    
+    
+CmdHelp("alive").add_command(
+  "alive", None, "To check am i alive"
+).add_command(
+  "mafia", None, "To check am i alive with your favorite alive pic"
+).add()
